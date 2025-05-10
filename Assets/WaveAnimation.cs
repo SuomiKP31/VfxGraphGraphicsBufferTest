aa8d844b2a12f86a83e539f0340423a828f1374a
RoarlisF/KP31 <fangzhx@shanghaitech.edu.cn>
2025-05-10 14:52:41 -0700

    CSV import

--- Assets/WaveAnimation.cs
+++ Assets/WaveAnimation.cs
@@ -7,12 +7,21 @@ public class WaveAnimation : MonoBehaviour
 
     GraphicsBuffer _buffer;
 
+    public int Count = 512 * 512;
+    
     void Start()
     {
         _buffer = new GraphicsBuffer
-          (GraphicsBuffer.Target.Structured, 128 * 128, 4 * sizeof(float));
+          (GraphicsBuffer.Target.Structured, 512*512, 4 * sizeof(float));
+        GetComponent<VisualEffect>().SetGraphicsBuffer("PointBuffer", _buffer);
+        
+        
+        _buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, Count, 4 * sizeof(float));
+
+
         GetComponent<VisualEffect>().SetGraphicsBuffer("PointBuffer", _buffer);
     }
+
 
     void OnDestroy()
     {
@@ -24,6 +33,15 @@ public class WaveAnimation : MonoBehaviour
     {
         _compute.SetFloat("Time", Time.time);
         _compute.SetBuffer(0, "PointBuffer", _buffer);
-        _compute.Dispatch(0, 16, 16, 1);
+        // _compute.Dispatch(0, 32, 32, 1);
+        
+        Vector4[] data = new Vector4[Count];
+        for (int i = 0; i < Count; i++)
+        {
+            data[i] = new Vector4(Random.value, Random.value, Random.value, Random.value);
+        }
+
+        _buffer.SetData(data);
+        
     }
 }
